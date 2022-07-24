229f3112a90d6abdcfae7ae6e692e264d0263667
Eunyeol <ney9083@gmail.com>
2022-07-24 22:33:07 +0900

    프론트엔드 개발 환경 세팅

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import Router from "./Router";
+
+function App() {
+  return (
+    <div>
+      <Router />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/Router.js
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
