cb159fe94d081e7428d3633aeb63af6ab0ab975b
Eunyeol <ney9083@gmail.com>
2022-07-26 11:25:56 +0900

    Etc: 개발 페이지 url 추가

---
+++ client/src/Routes/Main.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Main(props) {
+  return <div></div>;
+}
+
+export default Main;
---
+++ client/src/Routes/Register.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Register(props) {
+  return <div>회원가입</div>;
+}
+
+export default Register;
