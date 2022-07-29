5b39d5a8fd0471b08bf6300f1a9f0daf402be695
tarrot00 <tarrot00@gmail.com>
2022-07-30 00:55:00 +0900

    Fix:Footer 위치 변경 완료

--- client/src/Router.js
+++ client/src/Router.js
@@ -14,6 +14,7 @@ import AuctionDetail from './Routes/AuctionDetail';
 import PurchaseHistoryDetail from './Routes/PurchaseHistoryDetail';
 import Talks from './Routes/Talks';
 import NavigationBar from './components/ui/NavigationBar';
+import Footer from './components/ui/Footer';
 
 function Router() {
   return (
@@ -35,6 +36,7 @@ function Router() {
         <Route path="/auctions/:auctionsId" element={<AuctionDetail />} />
         <Route path="/puchase-history/purchseId" element={<PurchaseHistoryDetail />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
