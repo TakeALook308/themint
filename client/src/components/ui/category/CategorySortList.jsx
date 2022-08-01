9605bee1c56e9ed5419c507cc0f25270dea72089
kimsoohwan <soo930810@naver.com>
2022-08-01 10:08:28 +0900

    Feat: 정렬 기능 API 요청

--- client/src/components/ui/category/CategorySortList.jsx
+++ client/src/components/ui/category/CategorySortList.jsx
@@ -1,131 +1,36 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import axios from axios;
 
-function CategoryList(props) {
-
-
-  function sortByRecent(auctions.posttime) {
-    const sortedArr = arr.slice();
-    for (let i = 0; i < sortedArr.length; i++) {
-      let swap;
-      for (let j = 0; j < sortedArr.length - 1 - i; j++) {
-        if (sortedArr[j].itemPrice > sortedArr[j + 1].itemPrice) {
-          swap = sortedArr[j];
-          sortedArr[j] = sortedArr[j + 1];
-          sortedArr[j + 1] = swap;
-        }
-      }
-      if (!swap) {
-        break;
-      }
+function SortListBtn() {
+  const selectList = ['최신순', '가격순', '인기순', '판매자 신뢰도'];
+  const [SortBy, setSortBy] = useState('최신순');
+  const handleSortBy = (e) => {
+    setSortBy(e.target.value);
+  };
+  useEffect;(()=> {
+    axios.get("url"), {
+      params: key,      
     }
-    return sortedArr;
-  }
-
-  function sortByLowPrice(auctions.products.startPrice) {
-    const sortedArr = arr.slice();
-    for (let i = 0; i < sortedArr.length; i++) {
-      let swap;
-      for (let j = 0; j < sortedArr.length - 1 - i; j++) {
-        if (sortedArr[j].itemPrice > sortedArr[j + 1].itemPrice) {
-          swap = sortedArr[j];
-          sortedArr[j] = sortedArr[j + 1];
-          sortedArr[j + 1] = swap;
-        }
-      }
-      if (!swap) {
-        break;
-      }
-    }
-    return sortedArr;
-  }
-
-  function sortByLike(auctions.like) {
-    const sortedArr = arr.slice();
-    for (let i = 0; i < sortedArr.length; i++) {
-      let swap;
-      for (let j = 0; j < sortedArr.length - 1 - i; j++) {
-        if (sortedArr[j].itemPrice > sortedArr[j + 1].itemPrice) {
-          swap = sortedArr[j];
-          sortedArr[j] = sortedArr[j + 1];
-          sortedArr[j + 1] = swap;
-        }
-      }
-      if (!swap) {
-        break;
-      }
-    }
-    return sortedArr;
-  }
-
-  function sortByLike(auctions.like) {
-    const sortedArr = arr.slice();
-    for (let i = 0; i < sortedArr.length; i++) {
-      let swap;
-      for (let j = 0; j < sortedArr.length - 1 - i; j++) {
-        if (sortedArr[j].itemPrice > sortedArr[j + 1].itemPrice) {
-          swap = sortedArr[j];
-          sortedArr[j] = sortedArr[j + 1];
-          sortedArr[j + 1] = swap;
-        }
-      }
-      if (!swap) {
-        break;
-      }
-    }
-    return sortedArr;
-  }
-
-  function sortByTrust(auctions.trust) {
-    const sortedArr = arr.slice();
-    for (let i = 0; i < sortedArr.length; i++) {
-      let swap;
-      for (let j = 0; j < sortedArr.length - 1 - i; j++) {
-        if (sortedArr[j].itemPrice > sortedArr[j + 1].itemPrice) {
-          swap = sortedArr[j];
-          sortedArr[j] = sortedArr[j + 1];
-          sortedArr[j + 1] = swap;
-        }
-      }
-      if (!swap) {
-        break;
-      }
-    }
-    return sortedArr;
-  }
-
-  const sortedByRecent = sortByRecent(auctions);
-  const sortedByLowPrice = sortByLowPrice(auctions);
-  const sortedByLike = sortByLike(auctions);
-  const sortedByTrust = sortByTrust(auctions);
-
-  const [priceFilter, setPriceFilter] = useState('sortByRecent');
-  function handleChange(event) {
-    setPriceFilter(event.target.value);
-  }
+  }, [SortBy])
 
   return (
-    <div>
-      <select onChange={handleChange}>
-        <option value="sortByRecent">최신순</option>
-        <option value="sortByPrice">가격순</option>
-        <option value="sortByLike">인기순</option>
-        <option value="sortByTrust">신뢰도순</option>
-      </select>
-      <div>
-        {sortFilter === 'sortByRecent'
-          ? sortedByRecent.map((itmes) => <Item items={items} key={items.id} />)
-          : null}
-        {priceFilter === 'sortByPrice'
-          ? sortedByLowPrice.map((items) => <Item items={items} key={items.id} />)
-          : null}
-        {priceFilter === 'sortByLike'
-          ? sortedByLike.map((items) => <Item items={items} key={items.id} />)
-          : null}
-        {priceFilter === 'sortByTrust'
-          ? sortedByTrust.map((items) => <Item items={items} key={items.id} />)
-          : null}
-      </div>
-    </div>
+    <SortSelect onChange={handleSortBy} value={SortBy}>
+      {selectList.map((item) => (
+        <SortOption value={item} key={item} />
+      ))}
+    </SortSelect>
   );
 }
-export default CategoryList;
+
+export default SortListBtn;
+
+const SortSelect = styled.select`
+  position: relative;
+  width: 10%;
+`;
+
+const SortOption = styled.option`
+  background-color: ${(props) => props.theme.colors.mainBlack};
+  font-size: ${(props) => props.theme.fontSizes.p};
+`;
