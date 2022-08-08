9a8e65a092114c391ed24b7662be6db53b19997d
yb8350 <yb8350@naver.com>
2022-08-08 09:15:12 +0900

    Fix: merge 충돌 해결

--- client/src/Router.js
+++ client/src/Router.js
@@ -16,6 +16,7 @@ import Talks from './Routes/Talks';
 import NavigationBar from './components/ui/common/NavigationBar';
 import Footer from './components/ui/common/Footer';
 import FindPassword from './Routes/FindPassword';
+import Standby from './Routes/Standby';
 
 function Router() {
   return (
@@ -34,7 +35,7 @@ function Router() {
         <Route path="/accounts/phone-number" element={<AccountsPhoneNumber />} />
         <Route path="/accounts/withdrawl" element={<AccountsWithdrawl />} />
         <Route path="/streamings/:auctionId" element={<Streaming />} />
-        {/* <Route path="/standby/:auctionId" element={<Standby />} /> */}
+        <Route path="/standby/:auctionId" element={<Standby />} />
         <Route path="/talks" element={<Talks />} />
         <Route path="/auctions/:auctionsId" element={<AuctionDetail />} />
         <Route path="/puchase-history/:purchaseId" element={<PurchaseHistoryDetail />} />
