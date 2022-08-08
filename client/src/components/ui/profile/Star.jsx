80350596f78a38e5e929fe290064f9f236380f47
kimsoohwan <soo930810@naver.com>
2022-08-08 10:29:42 +0900

    Style:별 테두리 추가

--- client/src/components/ui/profile/Star.jsx
+++ client/src/components/ui/profile/Star.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BsStarFill, BsStarHalf } from 'react-icons/bs';
+import { BsStarFill, BsStarHalf, BsStar } from 'react-icons/bs';
 
 // 별점에 따라서 아이콘을 출력하는 함수입니다.
 
@@ -15,7 +15,7 @@ const StarRating = ({ rating, color = 'FFDA7B' }) => {
       {Array(Math.floor(5 - rating))
         .fill(2)
         .map((el, i) => (
-          <BsStarFill key={i} size="15" color="191919" />
+          <BsStar key={i} size="15" color="FFDA7B" />
         ))}
     </>
   );
