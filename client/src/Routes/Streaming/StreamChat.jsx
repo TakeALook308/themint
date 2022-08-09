421cdccbf2237e1b2cd0b0d8612dc4240788be1a
yb8350 <yb8350@naver.com>
2022-08-10 01:20:12 +0900

    Feat: 채팅 테스트

--- client/src/Routes/Streaming/StreamChat.jsx
+++ client/src/Routes/Streaming/StreamChat.jsx
@@ -1,74 +1,32 @@
 import React, { useState, useSelector, useEffect } from 'react';
-import SockJS from 'sockjs-client';
-import Stomp from 'stompjs';
+
 import styled from 'styled-components';
 
-function StreamChat() {
-  var sock = new SockJS('http://localhost:8081/api/ws-stomp');
-  let client = Stomp.over(sock);
-  let nickname = '민서';
-  let roomId = 'test';
+function StreamChat({ sendMessage, chat }) {
   const [chatMessage, setChatMessage] = useState('');
-  const [mm, setMm] = useState(['하이']);
-  useEffect(() => {
-    client.connect({}, () => {
-      console.log('Connected : ' + roomId);
-      client.send(
-        '/pub/chat/message',
-        {},
-        JSON.stringify({ type: 0, roomId: roomId, nickname: nickname, memberSeq: 1 }),
-      );
-
-      // Create Message
-      client.send(
-        `/pub/chat/message`,
-        {},
-        JSON.stringify({
-          type: 1,
-          roomId: roomId,
-          nickname: nickname,
-          message: chatMessage,
-          memberSeq: 1,
-        }),
-      );
-
-      client.subscribe('/sub/chat/room/' + roomId, function (message) {
-        const messagedto = JSON.parse(message.body);
-        setMm(...mm, messagedto);
-      });
-    });
-    return () => client.disconnect();
-  }, []);
+  console.log(chat);
   return (
     <Article>
       <ul>
-        {mm.map((item, i) => (
-          <li key={i}>{item}</li>
+        {chat.map((item, i) => (
+          <li key={i}>
+            {item.nickname} : {item.message}
+          </li>
         ))}
       </ul>
-      <p>{chatMessage}</p>
 
       <input
         type="text"
         placeholder="내용 입력"
-        // value={chatMessage}
-        // onChange={(e) => {
-        //   setChatMessage(e.target.value);
-        // }}
+        value={chatMessage}
+        onChange={(e) => {
+          setChatMessage(e.target.value);
+        }}
       />
       <button
         onClick={() => {
-          client.send(
-            `/pub/chat/message`,
-            {},
-            JSON.stringify({
-              type: 1,
-              roomId: roomId,
-              nickname: nickname,
-              message: chatMessage,
-              memberSeq: 1,
-            }),
-          );
+          sendMessage(chatMessage);
+          setChatMessage('');
         }}>
         send
       </button>
--- client/src/Routes/Streaming/StreamingPage.jsx
+++ client/src/Routes/Streaming/StreamingPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import StreamingComponent from '../../components/webRTC/StreamingComponent';
 import StreamingHeader from './StreamingHeader';
@@ -7,7 +7,51 @@ import AuctionBidding from './AuctionBidding';
 import AuctionList from './AuctionList';
 import { useRecoilValue } from 'recoil';
 import { myInformationState } from '../../atoms';
+import SockJS from 'sockjs-client';
+import Stomp from 'stompjs';
+
 function Streaming(props) {
+  var sock = new SockJS('http://localhost:8081/api/ws-stomp');
+  let client = Stomp.over(sock);
+  let nickname = '민서';
+  let roomId = 'test';
+  const [chat, setChat] = useState([]);
+
+  //처음 접속했을 때
+  useEffect(() => {
+    client.connect({}, () => {
+      console.log('Connected : ' + roomId);
+      //연결 후 데이터 가져오기
+      client.subscribe('/sub/chat/room/' + roomId, function (message) {
+        const messagedto = JSON.parse(message.body);
+        setChat([...chat, messagedto]);
+      });
+
+      //방 접속 알림 모두에게 쏴주기
+      client.send(
+        '/pub/chat/message',
+        {},
+        JSON.stringify({ type: 0, roomId: roomId, nickname: nickname, memberSeq: 1 }),
+      );
+    });
+    //종료
+    return () => client.disconnect();
+  }, []);
+
+  const sendMessage = (msg) => {
+    client.send(
+      `/pub/chat/message`,
+      {},
+      JSON.stringify({
+        type: 1,
+        roomId: roomId,
+        nickname: nickname,
+        message: msg,
+        memberSeq: 1,
+      }),
+    );
+  };
+
   const [nowProduct, setNowProduct] = useState(0);
   const [products, setProducts] = useState([
     {
@@ -21,7 +65,7 @@ function Streaming(props) {
       status: 0,
     },
   ]);
-
+  console.log(chat);
   const userInfo = useRecoilValue(myInformationState);
   const auctionData = { memberId: 'ney9083' };
 
@@ -38,7 +82,7 @@ function Streaming(props) {
         </Section>
         <Aside>
           <AuctionBidding product={products[nowProduct]} />
-          <StreamChat />
+          <StreamChat sendMessage={sendMessage} chat={chat} />
         </Aside>
       </Main>
     </Stream>
