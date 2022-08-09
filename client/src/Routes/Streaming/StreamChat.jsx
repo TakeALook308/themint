9c16608742af148026e402abe83a70912cc3001e
yb8350 <yb8350@naver.com>
2022-08-09 17:45:10 +0900

    Feat: 스트리밍 채팅

--- client/src/Routes/Streaming/StreamChat.jsx
+++ client/src/Routes/Streaming/StreamChat.jsx
@@ -1,27 +1,27 @@
 import React, { useState, useSelector, useEffect } from 'react';
 import SockJS from 'sockjs-client';
-import Stomp from '@stomp/stompjs';
+import Stomp from 'stompjs';
 import styled from 'styled-components';
 
 function StreamChat() {
-  var sock = new SockJS('/ws-stomp');
+  var sock = new SockJS('http://localhost:8081/api/ws-stomp');
   let client = Stomp.over(sock);
   let nickname = '민서';
   let roomId = 'test';
   const [chatMessage, setChatMessage] = useState('');
-
+  const [mm, setMm] = useState(['하이']);
   useEffect(() => {
     client.connect({}, () => {
       console.log('Connected : ' + roomId);
       client.send(
-        '/pub/api/chat/message',
+        '/pub/chat/message',
         {},
         JSON.stringify({ type: 0, roomId: roomId, nickname: nickname, memberSeq: 1 }),
       );
 
       // Create Message
       client.send(
-        `/pub/api/chat/message`,
+        `/pub/chat/message`,
         {},
         JSON.stringify({
           type: 1,
@@ -34,114 +34,32 @@ function StreamChat() {
 
       client.subscribe('/sub/chat/room/' + roomId, function (message) {
         const messagedto = JSON.parse(message.body);
+        setMm(...mm, messagedto);
       });
     });
     return () => client.disconnect();
-  }, [client]);
-
-  // let sock = new SockJS('/ws-stomp');
-  // let ws = Stomp.over(sock);
-  // let nickname = '민트';
-  // useEffect(() => {
-  //   console.log('Connected : ' + nickname);
-  //   ws.send('/pub/chat/message', {}, JSON.stringify(nickname));
-  // });
-
-  // 방 제목 가져오기
-  //  const { roomName, category } = useSelector((state) => state.chat.currentChat);
-  //  const roomId = useSelector((state) => state.chat.currentChat.roomId);
-  // const roomId = 'test';
-
-  // let reconnect = 0;
-  // const [mm, setMm] = useState({
-  //   roomId: 'test',
-  //   sender: '민트',
-  //   message: chatMessage,
-  //   messages: [],
-  //   sendMessage: () => {
-  //     ws.send(
-  //       '/pub/chat/message',
-  //       {},
-  //       JSON.stringify({
-  //         type: 'TALK',
-  //         roomId: this.roomId,
-  //         sender: this.sender,
-  //         message: this.message,
-  //       }),
-  //     );
-  //   },
-  //   recvMessage: (recv) => {
-  //     this.messages.unshift({
-  //       type: recv.type,
-  //       sender: recv.type == 'ENTER' ? '[알림]' : recv.sender,
-  //       message: recv.message,
-  //     });
-  //   },
-  // });
-
-  // const connect = () => {
-  //   ws.connect(
-  //     {},
-  //     function (frame) {
-  //       ws.subscribe('/sub/chat/room/' + mm.roomId, function (message) {
-  //         let recv = JSON.parse(message.body);
-  //         mm.recvMessage(recv);
-  //       });
-  //       ws.send(
-  //         '/pub/chat/message',
-  //         {},
-  //         JSON.stringify({ type: 'ENTER', roomId: mm.roomId, sender: mm.sender }),
-  //       );
-  //     },
-  //     function (error) {
-  //       if (reconnect++ <= 5) {
-  //         setTimeout(function () {
-  //           console.log('connection reconnect');
-  //           sock = new SockJS('/ws-stomp');
-  //           ws = Stomp.over(sock);
-  //           connect();
-  //         }, 10 * 1000);
-  //       }
-  //     },
-  //   );
-  // };
-
-  // const sendMessage = () => {
-  //   ws.send(
-  //     '/pub/chat/message',
-  //     {},
-  //     JSON.stringify({
-  //       type: 'TALK',
-  //       roomId: this.roomId,
-  //       sender: this.sender,
-  //       message: this.message,
-  //     }),
-  //   );
-  //   this.message = '';
-  // };
+  }, []);
   return (
     <Article>
-      {/* <ul>
-        {mm.messages.map((item, i) => (
-          <li key={i}>
-            {item.sender} : {item.message}
-          </li>
+      <ul>
+        {mm.map((item, i) => (
+          <li key={i}>{item}</li>
         ))}
       </ul>
       <p>{chatMessage}</p>
-       */}
+
       <input
         type="text"
         placeholder="내용 입력"
-        value={chatMessage}
-        onChange={(e) => {
-          setChatMessage(e.target.value);
-        }}
+        // value={chatMessage}
+        // onChange={(e) => {
+        //   setChatMessage(e.target.value);
+        // }}
       />
       <button
         onClick={() => {
           client.send(
-            `/pub/api/chat/message`,
+            `/pub/chat/message`,
             {},
             JSON.stringify({
               type: 1,
