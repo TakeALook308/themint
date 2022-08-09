d2a398d5c114e892d1d56c18ab0b26580fb27b10
Eunyeol <ney9083@gmail.com>
2022-08-09 14:33:54 +0900

    Feat: 로그아웃 기능 구현

--- client/src/Routes/Main/MainPage.jsx
+++ client/src/Routes/Main/MainPage.jsx
@@ -9,10 +9,13 @@ import { auctionListApis } from '../../utils/api/getAuctionApi';
 import AuctionCard from '../../components/CardList/AuctionCard';
 import SkeletonAuctionCard from '../../components/CardList/SkeletonAuctionCard';
 import { Container } from '../../style/common';
+import useLogout from '../../utils/hooks/useLogout';
 
 function MainPage(props) {
+  const logout = useLogout();
   return (
     <Container>
+      <button onClick={logout}>로그아웃</button>
       <Banner />
       <StreamList />
       <PostList />
