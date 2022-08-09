d3d8173962eb370612aaaf3ae8df696512676607
Eunyeol <ney9083@gmail.com>
2022-08-09 14:38:35 +0900

    Fix: 로그아웃 시 토큰이 사라지지 않는 문제 해결

--- client/src/utils/hooks/useLogout.jsx
+++ client/src/utils/hooks/useLogout.jsx
@@ -3,6 +3,7 @@ import { useRecoilState, useSetRecoilState } from 'recoil';
 import { session } from '../../App';
 import { loggedinState, myInformationState } from '../../atoms';
 import { infoToast } from '../../lib/toast';
+import { removeCookie } from '../functions/cookies';
 
 function useLogout() {
   const [myInformation, setMyinformation] = useRecoilState(myInformationState);
@@ -13,6 +14,7 @@ function useLogout() {
     infoToast(`${myInformation.nickname}님 다음에 또 오세요!`);
     setLoggedin(false);
     setMyinformation({ memberId: '', memberSeq: null, nickname: '' });
+    removeCookie('accessToken');
     session.remove('profile');
     navigate('/main');
   };
