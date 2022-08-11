ea344d0f334837a3714a94647f371f9549224e05
mino <etminho12@naver.com>
2022-08-11 16:29:45 +0900

    1:1 채팅 API 구현

--- server/src/main/java/com/takealook/api/service/ChatRoomMemberServiceImpl.java
+++ server/src/main/java/com/takealook/api/service/ChatRoomMemberServiceImpl.java
@@ -2,8 +2,10 @@ package com.takealook.api.service;
 
 import com.takealook.db.entity.ChatRoom;
 import com.takealook.db.entity.ChatRoomMember;
+import com.takealook.db.entity.Member;
 import com.takealook.db.repository.ChatRoomMemberRepository;
 import com.takealook.db.repository.ChatRoomRepository;
+import com.takealook.db.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,12 +17,16 @@ public class ChatRoomMemberServiceImpl implements ChatRoomMemberService{
     @Autowired
     ChatRoomRepository chatRoomRepository;
 
+    @Autowired
+    MemberRepository memberRepository;
+
     @Override
     public ChatRoomMember saveChatRoomMember(String roomId, Long memberSeq) {
+        Member member = memberRepository.findBySeq(memberSeq);
         ChatRoom chatRoomCheck = chatRoomRepository.getChatRoomByRoomId(roomId);
         ChatRoomMember duplicateCheck = chatRoomMemberRepository.getChatRoomMemberByMemberSeqAndRoomId(memberSeq, roomId);
         // 중복 입장 방지 or 생성된 채팅방 없음
-        if (duplicateCheck != null || chatRoomCheck == null)
+        if (duplicateCheck != null || chatRoomCheck == null || member == null)
             return null;
         ChatRoomMember chatRoomMember = ChatRoomMember.builder()
                 .roomId(roomId)
