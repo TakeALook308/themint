52b0e151c1f9779cc4d81deb00786a3f6c60f916
김혜성 <syong0716@naver.com>
2022-08-13 02:58:55 +0900

    Feat: 관심 경매 목록 페이징

--- server/src/main/java/com/takealook/api/controller/InterestController.java
+++ server/src/main/java/com/takealook/api/controller/InterestController.java
@@ -1,6 +1,7 @@
 package com.takealook.api.controller;
 
 import com.takealook.api.response.AuctionListEntityRes;
+import com.takealook.api.response.AuctionListRes;
 import com.takealook.api.response.InterestCategoryRes;
 import com.takealook.api.response.InterestKeywordRes;
 import com.takealook.api.service.*;
@@ -12,6 +13,7 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
@@ -134,18 +136,21 @@ public class InterestController {
     @ApiOperation(value = "멤버의 관심 경매 조회", notes = "로그인한 회원의 관심 경매 목록을 응답한다.")
     @ApiResponses({@ApiResponse(code = 200, message = "성공"), @ApiResponse(code = 401, message = "인증 실패"),
             @ApiResponse(code = 404, message = "사용자 없음"), @ApiResponse(code = 500, message = "서버 오류")})
-    public ResponseEntity<List<AuctionListEntityRes>> getInterestAuctionList(@ApiIgnore Authentication authentication) {
+    public ResponseEntity<AuctionListRes> getInterestAuctionList(@RequestParam("page") int page, @RequestParam("size") int size, @ApiIgnore Authentication authentication) {
         MemberDetails memberDetails = (MemberDetails) authentication.getDetails();
         Long memberSeq = memberDetails.getMemberSeq();
         List<AuctionListEntityRes> auctionListRes = new ArrayList<>();
-        List<Auction> auctionList = interestAuctionService.getInterestAuctionListByMemberSeq(memberSeq);
+        Boolean hasMore = false;
+        List<Auction> auctionList = interestAuctionService.getInterestAuctionListByMemberSeq(memberSeq, PageRequest.of(page, size));
+        List<Auction> hasMoreList = interestAuctionService.getInterestAuctionListByMemberSeq(memberSeq, PageRequest.of(page + 1, size));
+        if(hasMoreList.size() != 0) hasMore = true;
         for (Auction auction : auctionList) {
             Long auctionMemberSeq = auction.getMemberSeq();
             Member member = memberService.getMemberByMemberSeq(auctionMemberSeq);
             List<AuctionImage> auctionImageList = auctionImageService.getAuctionImageListByAuctionSeq(auction.getSeq());
             auctionListRes.add(AuctionListEntityRes.of(auction, member, auctionImageList));
         }
-        return ResponseEntity.status(200).body(auctionListRes);
+        return ResponseEntity.status(200).body(AuctionListRes.of(auctionListRes, false, 0L, hasMore));
     }
 
     @DeleteMapping("/auction/{hash}")
--- server/src/main/java/com/takealook/db/repository/InterestAuctionRepository.java
+++ server/src/main/java/com/takealook/db/repository/InterestAuctionRepository.java
@@ -1,6 +1,7 @@
 package com.takealook.db.repository;
 
 import com.takealook.db.entity.InterestAuction;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
@@ -11,7 +12,7 @@ import java.util.List;
 @Repository
 public interface InterestAuctionRepository extends JpaRepository<InterestAuction, Long> {
     InterestAuction findByMemberSeqAndHash(Long memberSeq, String hash);
-    List<InterestAuction> findAllByMemberSeq(Long memberSeq);
+    List<InterestAuction> findAllByMemberSeq(Long memberSeq, Pageable pageable);
     @Transactional // update, delete 필수
     @Modifying(clearAutomatically = true) // 영속성 컨텍스트 초기화
     int deleteByMemberSeqAndHash(Long memberSeq, String hash);
