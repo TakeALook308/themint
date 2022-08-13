aa114759b26f495ac1d87707c3d421f43e550d3c
Eunyeol <ney9083@gmail.com>
2022-08-13 20:32:38 +0900

    Style: console 주석 처리

--- client/src/Routes/Standby/StandbyPage.jsx
+++ client/src/Routes/Standby/StandbyPage.jsx
@@ -34,14 +34,14 @@ function StandbyPage() {
   const testSession = OV.initSession();
 
   const getToken = async (curSessionId) => {
-    console.log('===== 세션 연결 중 : ', curSessionId);
+    // console.log('===== 세션 연결 중 : ', curSessionId);
     return await createSession(curSessionId).then((sessionId) => createToken(sessionId));
   };
 
   const standbyJoin = (standbySession, nickName) => {
     // let standbySessionId = `${nickName}tests`;
     let standbySessionId = `${nickName}tests`;
-    console.log('스탠바이 세션 입장 ', standbySessionId);
+    // console.log('스탠바이 세션 입장 ', standbySessionId);
     getToken(standbySessionId).then((token) => {
       standbySession
         .connect(token, {})
@@ -56,7 +56,7 @@ function StandbyPage() {
             insertMode: 'APPEND',
             mirror: false,
           });
-          console.log('video active', pub.stream.videoActive);
+          // console.log('video active', pub.stream.videoActive);
           standbySession.publish(pub);
           setPublisher(pub);
         })
@@ -85,7 +85,7 @@ function StandbyPage() {
           },
         })
         .then((response) => {
-          console.log('CREATE SESION', response);
+          // console.log('CREATE SESION', response);
           resolve(response.data.id);
         })
         .catch((response) => {
