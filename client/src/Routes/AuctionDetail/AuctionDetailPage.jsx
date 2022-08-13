4c8583d71c6a69766c301f021e73e1df1d969750
yb8350 <yb8350@naver.com>
2022-08-14 01:49:57 +0900

    Feat: 경매 디테일 상단 구현

--- client/src/Routes/AuctionDetail/AuctionDetailPage.jsx
+++ client/src/Routes/AuctionDetail/AuctionDetailPage.jsx
@@ -14,6 +14,8 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 
 function AuctionDetailPage(props) {
+  const userInfo = useRecoilValue(myInformationState);
+  const navigate = useNavigate();
   const params = useParams();
   const [auctionInfo, setAuctionInfo] = useState(null);
   const ff = async () => {
@@ -29,11 +31,10 @@ function AuctionDetailPage(props) {
     ff();
   }, []);
 
-  const userInfo = useRecoilValue(myInformationState);
-  const navigate = useNavigate();
   if (auctionInfo) {
     return (
       <Container>
+        {console.log(auctionInfo)}
         <AuctionMainInfo>
           <AuctionImage>
             <Swiper
@@ -49,18 +50,28 @@ function AuctionDetailPage(props) {
                   <SwiperSlide key={i}>
                     <img
                       src={`https://s3.ap-northeast-2.amazonaws.com/s3-themint${item.imageUrl}`}
-                      alt=""
-                      width="100%"
+                      alt={`이미지${i}`}
                     />
                   </SwiperSlide>
                 );
               })}
             </Swiper>
           </AuctionImage>
-          <div>
-            {/* <p>{categories[auctionInfo.categorySeq-1].name}</p> */}
-            <p>{auctionInfo.title}</p>
+          <AuctionInfoBox>
             <div>
+              <AuctionTitleLeft>
+                <p className="category">{categories[auctionInfo.categorySeq - 1].name}</p>
+                <p className="title">{auctionInfo.title}</p>
+              </AuctionTitleLeft>
+              <AuctionTitleRight>
+                <div>알림</div>
+              </AuctionTitleRight>
+            </div>
+            <div
+              className="profile"
+              onClick={() => {
+                navigate(`/profile/${auctionInfo.memberSeq}`);
+              }}>
               <img
                 src={`https://s3.ap-northeast-2.amazonaws.com/s3-themint${auctionInfo.profileUrl}`}
                 alt="프로필 이미지"
@@ -68,19 +79,32 @@ function AuctionDetailPage(props) {
               />
               <span>{auctionInfo.nickname}</span>
             </div>
-          </div>
-          <div>
-            <span>{auctionInfo.startTime}</span>
-          </div>
-          <GradientButton
-            text="경매 참여"
-            onClick={() => {
-              if (auctionInfo.memberSeq === userInfo.memberSeq)
-                navigate(`/standby/${params.auctionId}`);
-              else navigate(`/streamings/${params.auctionId}`);
-            }}></GradientButton>
+            <div className="date">
+              <p>{auctionInfo.startTime.slice(0, -3)} 예정</p>
+              {auctionInfo.memberSeq === userInfo.memberSeq ? (
+                <div>
+                  <span>수정</span>
+                  <span>삭제</span>
+                </div>
+              ) : null}
+            </div>
+            <GradientButton
+              text="경매 참여"
+              onClick={() => {
+                if (auctionInfo.memberSeq === userInfo.memberSeq)
+                  navigate(`/standby/${params.auctionId}`);
+                else navigate(`/streamings/${params.auctionId}`);
+              }}></GradientButton>
+          </AuctionInfoBox>
         </AuctionMainInfo>
-        {/* <AuctionList></AuctionList> */}
+        <AuctionList>
+          {auctionInfo.productList.map((item, i) => (
+            <div key={i}>
+              <p>{item.productName}</p>
+              <span>{item.startPrice}</span>
+            </div>
+          ))}
+        </AuctionList>
         <AuctionContent>
           <p>{auctionInfo.content}</p>
         </AuctionContent>
@@ -88,14 +112,85 @@ function AuctionDetailPage(props) {
     );
   } else return <Container>상품이 존재하지 않습니다.</Container>;
 }
-
-const AuctionImage = styled.div`
-  width: 40%;
-`;
-
 const AuctionMainInfo = styled.div`
   display: flex;
-  height: 300px;
+  width: 100%;
+  height: 350px;
+  @media screen and (max-width: 768px) {
+    flex-direction: column;
+    height: auto;
+  }
+`;
+const AuctionImage = styled.div`
+  width: 55%;
+  padding: 10px;
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    height: 350px;
+  }
+`;
+
+const AuctionInfoBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 45%;
+  padding: 20px 10px;
+  gap: 10px;
+  justify-content: space-between;
+  & > div {
+    display: flex;
+    justify-content: space-between;
+  }
+  .profile {
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    img {
+      border-radius: 15px;
+    }
+    span {
+      display: inline-block;
+      padding-left: 10px;
+    }
+  }
+  .date {
+    flex-grow: 1;
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-end;
+    padding-bottom: 10px;
+    div {
+      padding-right: 5px;
+      display: flex;
+      gap: 10px;
+      span {
+        text-decoration: underline;
+        cursor: pointer;
+      }
+    }
+  }
+  @media screen and (max-width: 768px) {
+    width: 100%;
+  }
+`;
+
+const AuctionTitleLeft = styled.div`
+  .category {
+    font-size: 14px;
+    padding: 10px 0;
+  }
+  .title {
+    font-size: 28px;
+    font-weight: 700;
+    padding-bottom: 10px;
+  }
+`;
+
+const AuctionTitleRight = styled.div``;
+
+const AuctionList = styled.div`
+  display: flex;
+  width: 100%;
 `;
 
 const AuctionContent = styled.div``;
