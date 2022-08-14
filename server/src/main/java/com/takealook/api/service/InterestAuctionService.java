b7d1a8ba5a52ad8aa001639957318827677440fb
김혜성 <syong0716@naver.com>
2022-08-14 15:29:33 +0900

    Feat: 경매 상세 조회에서 멤버의 관심 여부 추가

--- server/src/main/java/com/takealook/api/service/InterestAuctionService.java
+++ server/src/main/java/com/takealook/api/service/InterestAuctionService.java
@@ -9,4 +9,5 @@ public interface InterestAuctionService {
     void createInterestAuction(Long memberSeq, String hash);
     List<Auction> getInterestAuctionListByMemberSeq(Long memberSeq, Pageable pageable);
     void deleteAuction(Long memberSeq, String hash);
+    Boolean checkInterestByMemberSeq(Long memberSeq, String auctionHash);
 }
