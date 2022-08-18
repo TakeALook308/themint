b84a3b5e1ea2b9418799b8c757abd2cd8391dd68
Eunyeol <ney9083@gmail.com>
2022-08-18 17:33:04 +0900

    Etc: 불필요한파일 라이브러리삭제

--- client/src/components/CardList/AuctionCard.jsx
+++ client/src/components/CardList/AuctionCard.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-import { resizeFile } from '../../utils/functions/resizeFile';
 
 function AuctionCard({ auction }) {
   const [auctionTime, setAuctionTime] = useState({ moreThenOneDay: false, time: '' });
