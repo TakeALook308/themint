ff135f96b09c3f982a017cd718255009cdfb102c
Eunyeol <ney9083@gmail.com>
2022-08-18 22:07:40 +0900

    Style: 사용하지 않는 import 삭제

--- client/src/Routes/Talks/TalkCard.jsx
+++ client/src/Routes/Talks/TalkCard.jsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
-import { useRecoilValue } from 'recoil';
 import SockJS from 'sockjs-client';
 import Stomp from 'stompjs';
 import styled from 'styled-components';
