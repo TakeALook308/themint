8ecaf9b633305e296fb5a7cdd1fba7ab2782c38a
Eunyeol <ney9083@gmail.com>
2022-08-24 01:57:18 +0900

    Etc: git pull

--- client/src/Routes/AuctionCreate/AuctionCreatePage.jsx
+++ client/src/Routes/AuctionCreate/AuctionCreatePage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 import { Container, Title } from '../../style/style';
 import { categories } from '../../utils/constants/constant';
