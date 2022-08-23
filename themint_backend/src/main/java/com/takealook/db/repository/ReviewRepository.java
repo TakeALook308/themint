a88036d2c17540b53e79a5a2638c2ec144d7002e
김혜성 <syong0716@naver.com>
2022-08-23 17:33:22 +0900

    Feat: review 조회 api 구현

--- themint_backend/src/main/java/com/takealook/db/repository/ReviewRepository.java
+++ themint_backend/src/main/java/com/takealook/db/repository/ReviewRepository.java
@@ -5,9 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     List<Review> findAllByReceiverSeqOrderByDateDesc(Long memberSeq);
-    Review findByReceiverSeqAndProductSeq(Long memberseq, Long productSeq);
+    Optional<Review> findByReceiverSeqAndProductSeq(Long memberseq, Long productSeq);
 }
